96021fca70d9c989b71dce9eb693b471ef9cafdf
Herb Huang <hwongbaul@outlook.com>
2025-08-13 15:01:00 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴",
+    pageTitle: "Herb's Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
