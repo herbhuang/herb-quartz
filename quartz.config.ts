66deed9cb82e1abac52dd46db1a2537c77dd80ae
Herb Huang <hwongbaul@outlook.com>
2025-08-13 14:34:33 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,6 +13,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: {
       provider: 'umani',
+      host: 'https://cloud.umami.is',
       websiteId: 'f1e6bc67-94ea-4d16-8609-e8de5f413c3e',
     },
     locale: "en-US",
