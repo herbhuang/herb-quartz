9b0c3bf3b6e5a4c14905d931ace5cecc8577e30a
Herb Huang <hwongbaul@outlook.com>
2025-08-13 23:16:41 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -51,22 +51,22 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: false,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
-  right: [],
-}
+// // components for pages that display lists of pages  (e.g. tags or folders)
+// export const defaultListPageLayout: PageLayout = {
+//   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+//   left: [
+//     Component.PageTitle(),
+//     Component.MobileOnly(Component.Spacer()),
+//     Component.Flex({
+//       components: [
+//         {
+//           Component: Component.Search(),
+//           grow: false,
+//         },
+//         { Component: Component.Darkmode() },
+//       ],
+//     }),
+//     Component.Explorer(),
+//   ],
+//   right: [],
+// }
